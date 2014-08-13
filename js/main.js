ece2c6b8898730da8e7d7dac73550251e909eb4b
Meinte Boersma <meinte.boersma@gmail.com>
2014-08-13 21:45:37 +0200

    more clean-up (but introduced global state)

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function roundVal(temp) {
 }
 
 function kmh2beaufort(kmh) {
-	var speeds = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
+	var speeds = [1, 5.5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
 	for (var beaufort in speeds) {
 		var speed = speeds[beaufort];
 		if (speed > kmh) {
@@ -55,9 +55,7 @@ function addWeatherClass(elt, weather, small) {
 		'50n':'wi-night-alt-cloudy-windy'
 	};
 
-	var iconClass = iconTable[weather.icon];
-	elt.addClass('icon' + (!!small ? '-small' : '')).addClass('dimmed').addClass('wi').addClass(iconClass);
-	return elt;
+	return elt.addClass('icon' + (!!small ? '-small' : '')).addClass('dimmed').addClass('wi').addClass(iconTable[weather.icon]);
 }
 
 
@@ -66,38 +64,159 @@ function repeat(func, interval) {
 	setInterval(func, interval);
 }
 
+function checkVersion() {
+	$.getJSON('githash.php', {}, function(json, textStatus) {
+		if (json) {
+			if (json.gitHash != gitHash) {
+				window.location.reload();
+				window.location.href = window.location.href;
+			}
+		}
+	});
+}
+
+function updateTime() {
+    var now = moment();
+    var date = now.format('LLLL').split(' ',4);
+    date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
+
+	$('.date').html(date);
+	$('.time').html(now.format('HH') + ':' + now.format('mm') + '<span class="sec">' + now.format('ss') + '</span>');
+}
+
+function updateCalendar() {
+	table = $('<table/>').addClass('xsmall').addClass('calendar-table');
+	opacity = 1;
+
+
+	for (var i in eventList) {
+		var e = eventList[i];
+
+		var row = $('<tr/>').css('opacity',opacity);
+		row.append($('<td/>').html(e.description).addClass('description'));
+		row.append($('<td/>').html(e.days).addClass('days dimmed'));
+		table.append(row);
+
+		opacity -= 1 / eventList.length;
+	}
+
+	$('.calendar').updateWithText(table,1000);
+}
+
+function updateCompliment() {
+    //see compliments.js
+	while (compliment == lastCompliment) {
+		compliment = Math.floor(Math.random()*compliments.length);
+	}
+
+	$('.compliment').updateWithText(compliments[compliment], 4000);
+
+	lastCompliment = compliment;
+}
+
+function updateCurrentWeather() {
+	$.getJSON('http://api.openweathermap.org/data/2.5/weather', weatherParams, function(json, textStatus) {
+
+		var temp = roundVal(json.main.temp);
+
+		var wind = roundVal(json.wind.speed);
+
+		var icon = addWeatherClass($('<span/>'), json.weather[0]);
+		$('.temp').updateWithText(icon.outerHTML()+temp+'&deg;', 1000);
+
+		var now = new Date();
+		var sunrise = new Date(json.sys.sunrise*1000).toTimeString().substring(0,5);
+		var sunset = new Date(json.sys.sunset*1000).toTimeString().substring(0,5);
+
+		var windString = '<span class="wi wi-strong-wind xdimmed"></span> ' + kmh2beaufort(wind) ;
+		var sunString = '<span class="wi wi-sunrise xdimmed"></span> ' + sunrise;
+		if (json.sys.sunrise*1000 < now && json.sys.sunset*1000 > now) {
+			sunString = '<span class="wi wi-sunset xdimmed"></span> ' + sunset;
+		}
+
+		$('.windsun').updateWithText(windString+' '+sunString, 1000);
+	});
+}
+
+function updateWeatherForecast() {
+	$.getJSON('http://api.openweathermap.org/data/2.5/forecast', weatherParams, function(json, textStatus) {
+
+		var forecastData = {};
+
+		for (var i in json.list) {
+			var forecast = json.list[i];
+			var dateKey  = forecast.dt_txt.substring(0, 10);
+
+			if (forecastData[dateKey] == undefined) {
+				forecastData[dateKey] = {
+					'timestamp':forecast.dt * 1000,
+					'temp_min':forecast.main.temp,
+					'temp_max':forecast.main.temp,
+					'weather': {}
+				};
+			} else {
+				forecastData[dateKey]['temp_min'] = (forecast.main.temp < forecastData[dateKey]['temp_min']) ? forecast.main.temp : forecastData[dateKey]['temp_min'];
+				forecastData[dateKey]['temp_max'] = (forecast.main.temp > forecastData[dateKey]['temp_max']) ? forecast.main.temp : forecastData[dateKey]['temp_max'];
+				forecastData[dateKey]['weather'] = (forecast.weather[0] || forecastData[dateKey]['weather']);
+			}
+		}
+
+		var forecastTable = $('<table />').addClass('forecast-table');
+		var opacity = 1;
+		for (var i in forecastData) {
+			var forecast = forecastData[i];
+			var dt = new Date(forecast.timestamp);
+			var row = $('<tr />').css('opacity', opacity);
+	
+			row.append($('<td/>').addClass('day').html(moment.weekdaysShort(dt.getDay())));
+			row.append($('<td/>').addClass('temp-max').html(roundVal(forecast.temp_max)));
+			row.append($('<td/>').addClass('temp-min').html(roundVal(forecast.temp_min)));
+			row.append($('<td/>').append(addWeatherClass($('<span/>'), forecast.weather, true)));
+	
+			forecastTable.append(row);
+			opacity -= 0.155;
+		}
+
+		$('.forecast').updateWithText(forecastTable, 1000);
+	});
+}
+
+var eventList = [];
+
+var news = [];
+var newsIndex = 0;
+
+var lastCompliment;
+var compliment;
+
+function fetchNews() {
+	$.feedToJson({
+		feed:'http://feeds.nos.nl/nosjournaal?format=rss',
+		//feed:'http://www.nu.nl/feeds/rss/achterklap.rss',
+		//feed:'http://www.nu.nl/feeds/rss/opmerkelijk.rss',
+		success: function(data){
+			news = [];
+			for (var i in data.item) {
+				var item = data.item[i];
+				news.push(item.title);
+			}
+		}
+	});
+}
+
+function showNews() {
+	var newsItem = news[newsIndex];
+	$('.news').updateWithText(newsItem, 2000);
+
+	newsIndex--;
+	if (newsIndex < 0) newsIndex = news.length - 1;
+}
 
 $(function() {
-
-	var news = [];
-	var newsIndex = 0;
-
-	var eventList = [];
-
-	var lastCompliment;
-	var compliment;
-
     moment.lang(lang);
 
-	repeat(function checkVersion() {
-		$.getJSON('githash.php', {}, function(json, textStatus) {
-			if (json) {
-				if (json.gitHash != gitHash) {
-					window.location.reload();
-					window.location.href = window.location.href;
-				}
-			}
-		});
-	}, 3000);
-
-	repeat(function updateTime() {
-        var now = moment();
-        var date = now.format('LLLL').split(' ',4);
-        date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
-
-		$('.date').html(date);
-		$('.time').html(now.format('HH') + ':' + now.format('mm') + '<span class="sec">' + now.format('ss') + '</span>');
-	}, 1000);
+	repeat(checkVersion, 3000);
+	repeat(updateTime, 1000);
 
 	(function updateCalendarData() {
 		new ical_parser("calendar.php", function(cal) {
@@ -152,132 +271,11 @@ $(function() {
     	});
 	})();
 
-	repeat(function updateCalendar() {
-		table = $('<table/>').addClass('xsmall').addClass('calendar-table');
-		opacity = 1;
-
-
-		for (var i in eventList) {
-			var e = eventList[i];
-
-			var row = $('<tr/>').css('opacity',opacity);
-			row.append($('<td/>').html(e.description).addClass('description'));
-			row.append($('<td/>').html(e.days).addClass('days dimmed'));
-			table.append(row);
-
-			opacity -= 1 / eventList.length;
-		}
-
-		$('.calendar').updateWithText(table,1000);
-	}, 1000);
-
-	repeat(function updateCompliment() {
-        //see compliments.js
-		while (compliment == lastCompliment) {
-			compliment = Math.floor(Math.random()*compliments.length);
-		}
-
-		$('.compliment').updateWithText(compliments[compliment], 4000);
-
-		lastCompliment = compliment;
-	}, 30000);
-
-	repeat(function updateCurrentWeather() {
-		$.getJSON('http://api.openweathermap.org/data/2.5/weather', weatherParams, function(json, textStatus) {
-
-			var temp = roundVal(json.main.temp);
-			var temp_min = roundVal(json.main.temp_min);
-			var temp_max = roundVal(json.main.temp_max);
-
-			var wind = roundVal(json.wind.speed);
-
-			var icon = addWeatherClass($('<span/>'), json.weather[0]);
-			$('.temp').updateWithText(icon.outerHTML()+temp+'&deg;', 1000);
-
-			// var forecast = 'Min: '+temp_min+'&deg;, Max: '+temp_max+'&deg;';
-			// $('.forecast').updateWithText(forecast, 1000);
-
-			var now = new Date();
-			var sunrise = new Date(json.sys.sunrise*1000).toTimeString().substring(0,5);
-			var sunset = new Date(json.sys.sunset*1000).toTimeString().substring(0,5);
-
-			var windString = '<span class="wi wi-strong-wind xdimmed"></span> ' + kmh2beaufort(wind) ;
-			var sunString = '<span class="wi wi-sunrise xdimmed"></span> ' + sunrise;
-			if (json.sys.sunrise*1000 < now && json.sys.sunset*1000 > now) {
-				sunString = '<span class="wi wi-sunset xdimmed"></span> ' + sunset;
-			}
-
-			$('.windsun').updateWithText(windString+' '+sunString, 1000);
-		});
-	}, 60000);
-
-	repeat(function updateWeatherForecast() {
-			$.getJSON('http://api.openweathermap.org/data/2.5/forecast', weatherParams, function(json, textStatus) {
-
-			var forecastData = {};
-
-			for (var i in json.list) {
-				var forecast = json.list[i];
-				var dateKey  = forecast.dt_txt.substring(0, 10);
-
-				if (forecastData[dateKey] == undefined) {
-					forecastData[dateKey] = {
-						'timestamp':forecast.dt * 1000,
-						'temp_min':forecast.main.temp,
-						'temp_max':forecast.main.temp,
-						'weather': {}
-					};
-				} else {
-					forecastData[dateKey]['temp_min'] = (forecast.main.temp < forecastData[dateKey]['temp_min']) ? forecast.main.temp : forecastData[dateKey]['temp_min'];
-					forecastData[dateKey]['temp_max'] = (forecast.main.temp > forecastData[dateKey]['temp_max']) ? forecast.main.temp : forecastData[dateKey]['temp_max'];
-					forecastData[dateKey]['weather'] = (forecast.weather[0] || forecastData[dateKey]['weather']);
-				}
-
-			}
-
-
-			var forecastTable = $('<table />').addClass('forecast-table');
-			var opacity = 1;
-			for (var i in forecastData) {
-				var forecast = forecastData[i];
-				var dt = new Date(forecast.timestamp);
-				var row = $('<tr />').css('opacity', opacity);
-
-				row.append($('<td/>').addClass('day').html(moment.weekdaysShort(dt.getDay())));
-				row.append($('<td/>').addClass('temp-max').html(roundVal(forecast.temp_max)));
-				row.append($('<td/>').addClass('temp-min').html(roundVal(forecast.temp_min)));
-				row.append($('<td/>').append(addWeatherClass($('<span/>'), forecast.weather, true)));
-
-				forecastTable.append(row);
-				opacity -= 0.155;
-			}
-
-
-			$('.forecast').updateWithText(forecastTable, 1000);
-		});
-	}, 60000);
-
-	repeat(function fetchNews() {
-		$.feedToJson({
-			feed:'http://feeds.nos.nl/nosjournaal?format=rss',
-			//feed:'http://www.nu.nl/feeds/rss/achterklap.rss',
-			//feed:'http://www.nu.nl/feeds/rss/opmerkelijk.rss',
-			success: function(data){
-				news = [];
-				for (var i in data.item) {
-					var item = data.item[i];
-					news.push(item.title);
-				}
-			}
-		});
-	}, 60000);
-
-	repeat(function showNews() {
-		var newsItem = news[newsIndex];
-		$('.news').updateWithText(newsItem,2000);
-
-		newsIndex--;
-		if (newsIndex < 0) newsIndex = news.length - 1;
-	}, 5500);
+	repeat(updateCalendar, 1000);
+	repeat(updateCompliment, 30000);
+	repeat(updateCurrentWeather, 60000);
+	repeat(fetchNews, 60000);
+	repeat(showNews, 5500);
+	repeat(updateWeatherForecast, 60000);
 
 });
