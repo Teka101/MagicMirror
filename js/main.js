0086b96b82428da1d9d38675c3ef506d61011b1c
Meinte Boersma <meinte.boersma@gmail.com>
2014-08-13 22:02:18 +0200

    more restructuring/clean-up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+$(function() {
+    moment.lang(lang);
+
+    repeat(checkVersion, 3000);
+	repeat(updateTime, 1000);
+	repeat(updateCalendarData, 60000);
+	repeat(updateCalendar, 1000);
+	repeat(updateCompliment, 30000);
+	repeat(updateCurrentWeather, 60000);
+	repeat(fetchNews, 60000);
+	repeat(showNews, 5500);
+	repeat(updateWeatherForecast, 60000);
+
+});
+
+
+// global state:
+
+var eventList = [];
+
+var news = [];
+var newsIndex = 0;
+
+var lastCompliment;
+var compliment;
+
+
 jQuery.fn.updateWithText = function(text, speed) {
 	var dummy = $('<div/>').html(text);
 
@@ -83,6 +110,56 @@ function updateTime() {
 	$('.date').html(date);
 	$('.time').html(now.format('HH') + ':' + now.format('mm') + '<span class="sec">' + now.format('ss') + '</span>');
 }
+
+function updateCalendarData() {
+	new ical_parser("calendar.php", function(cal) {
+    	events = cal.getEvents();
+    	eventList = [];
+
+    	for (var i in events) {
+    		var e = events[i];
+    		for (var key in e) {
+    			var value = e[key];
+				var seperator = key.search(';');
+				if (seperator >= 0) {
+					var mainKey = key.substring(0,seperator);
+					var subKey = key.substring(seperator+1);
+
+					var dt;
+					if (subKey == 'VALUE=DATE') {
+						//date
+						dt = new Date(value.substring(0,4), value.substring(4,6) - 1, value.substring(6,8));
+					} else {
+						//time
+						dt = new Date(value.substring(0,4), value.substring(4,6) - 1, value.substring(6,8), value.substring(9,11), value.substring(11,13), value.substring(13,15));
+					}
+
+					if (mainKey == 'DTSTART') e.startDate = dt;
+					if (mainKey == 'DTEND') e.endDate = dt;
+				}
+    		}
+
+    		var seconds, startDate;
+            if (e.startDate == undefined){
+                //some old events in Gmail Calendar is "start_date"
+                //FIXME: problems with Gmail's TimeZone
+        		seconds = moment(e.DTSTART).diff(moment(), 'seconds');
+                startDate = moment(e.DTSTART);
+            } else {
+        		seconds = moment(e.startDate).diff(moment(), 'seconds');
+                startDate = moment(e.startDate);
+            }
+
+    		//only add fututre events, days doesn't work, we need to check seconds
+    		if (seconds >= 0) {
+    			var time_string = (seconds <= 60*60*5 || seconds >= 60*60*24*2) ? moment(startDate).fromNow() : moment(startDate).calendar();
+        		eventList.push({ 'description': e.SUMMARY, 'seconds': seconds, 'days': time_string });
+    		}
+    	};
+    	eventList.sort(function(a, b) { return a.seconds-b.seconds; });
+	});
+}
+
 
 function updateCalendar() {
 	table = $('<table/>').addClass('xsmall').addClass('calendar-table');
@@ -181,14 +258,6 @@ function updateWeatherForecast() {
 	});
 }
 
-var eventList = [];
-
-var news = [];
-var newsIndex = 0;
-
-var lastCompliment;
-var compliment;
-
 function fetchNews() {
 	$.feedToJson({
 		feed:'http://feeds.nos.nl/nosjournaal?format=rss',
@@ -212,70 +281,3 @@ function showNews() {
 	if (newsIndex < 0) newsIndex = news.length - 1;
 }
 
-$(function() {
-    moment.lang(lang);
-
-	repeat(checkVersion, 3000);
-	repeat(updateTime, 1000);
-
-	(function updateCalendarData() {
-		new ical_parser("calendar.php", function(cal) {
-        	events = cal.getEvents();
-        	eventList = [];
-
-        	for (var i in events) {
-        		var e = events[i];
-        		for (var key in e) {
-        			var value = e[key];
-					var seperator = key.search(';');
-					if (seperator >= 0) {
-						var mainKey = key.substring(0,seperator);
-						var subKey = key.substring(seperator+1);
-
-						var dt;
-						if (subKey == 'VALUE=DATE') {
-							//date
-							dt = new Date(value.substring(0,4), value.substring(4,6) - 1, value.substring(6,8));
-						} else {
-							//time
-							dt = new Date(value.substring(0,4), value.substring(4,6) - 1, value.substring(6,8), value.substring(9,11), value.substring(11,13), value.substring(13,15));
-						}
-
-						if (mainKey == 'DTSTART') e.startDate = dt;
-						if (mainKey == 'DTEND') e.endDate = dt;
-					}
-        		}
-
-        		var seconds, startDate;
-                if (e.startDate == undefined){
-                    //some old events in Gmail Calendar is "start_date"
-                    //FIXME: problems with Gmail's TimeZone
-            		seconds = moment(e.DTSTART).diff(moment(), 'seconds');
-                    startDate = moment(e.DTSTART);
-                } else {
-            		seconds = moment(e.startDate).diff(moment(), 'seconds');
-                    startDate = moment(e.startDate);
-                }
-
-        		//only add fututre events, days doesn't work, we need to check seconds
-        		if (seconds >= 0) {
-        			var time_string = (seconds <= 60*60*5 || seconds >= 60*60*24*2) ? moment(startDate).fromNow() : moment(startDate).calendar();
-	        		eventList.push({ 'description': e.SUMMARY, 'seconds': seconds, 'days': time_string });
-        		}
-        	};
-        	eventList.sort(function(a, b) { return a.seconds-b.seconds; });
-
-        	setTimeout(function() {
-        		updateCalendarData();
-        	}, 60000);
-    	});
-	})();
-
-	repeat(updateCalendar, 1000);
-	repeat(updateCompliment, 30000);
-	repeat(updateCurrentWeather, 60000);
-	repeat(fetchNews, 60000);
-	repeat(showNews, 5500);
-	repeat(updateWeatherForecast, 60000);
-
-});
